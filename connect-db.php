9ffb801f3c33536aed11db455003446bee877e0e
Ruolin <rlc8my@vriginia.edu>
2019-04-29 20:33:18 -0400

    Updates

---
+++ connect-db.php
@@ -0,0 +1,21 @@
+<?php 
+$hostname = 'localhost';
+$dbname = 'id9306054_kpopkommunity';
+$username = 'id9306054_kpopkommunity';
+$password = 'cs4640sp19';
+$dsn = "mysql:host=$hostname;dbname=id9306054_kpopkommunity";
+try {
+	$db = new PDO($dsn, $username, $password);
+}
+
+catch (PDOException $e) {
+	$error_message = $e -> getMessage();
+	echo "<p>An error occured while connnecting to the database: $error_message </p>";
+}
+
+catch (Exception $e) {
+    $error_message = $e->getMessage();
+    echo "<p>Error message: $error_message </p>";
+}
+	
+?>
